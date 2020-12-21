4bb3ea073b57e4b712d8a3ee87a647fbcc906bd9
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2020-12-21 17:51:15 +0800

    UI 微調

---
+++ MapDesigner/Helpers/FileManageHelper.cs
@@ -0,0 +1,9 @@
+namespace MapDesigner.Helpers{
+    public interface IFileManageHelper{
+        public dynamic readFile();
+        public dynamic 
+    }
+    public class FileManageHelper{
+
+    }
+}
